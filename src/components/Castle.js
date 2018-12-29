f9a4e53633aa216852a356a569a7763405fca67f
Yoan Ante <yoanante@gmail.com>
2018-12-28 22:49:38 -0500

    rendering castles

--- src/components/Castle.js
+++ src/components/Castle.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Castle() {
+export default function Castle(props) {
   return (
-    <div>
-      I am a motherfucking castle
+    <div className="castle-card">
+      <h2 className="castle-card-header">{props.castle.name}</h2>
+      <img src={props.castle.image} alt={props.castle.name}/>
+
     </div>
   )
 }
+
+
