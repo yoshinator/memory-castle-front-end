4e77e869e8810c7c20b1b52385259c1ad47742e0
Yoan Ante <yoanante@gmail.com>
2018-12-29 18:17:09 -0500

    functional to class component also added delete and expand button. Need to finish handleExpand

--- src/components/Castle.js
+++ src/components/Castle.js
@@ -1,13 +1,31 @@
-import React from 'react'
 
-export default function Castle(props) {
-  return (
-    <div className="castle-card">
-      <h2 className="castle-card-header">{props.castle.name}</h2>
-      <img src={props.castle.image} alt={props.castle.name}/>
+import React, { Component } from 'react'
+import JSONAPIAdapter from "../JSONAPIAdpater";
+const ApiAdapter = new JSONAPIAdapter("api/v1/castles");
 
-    </div>
-  )
+export default class Castle extends Component {
+
+  handleExpand = () => {
+
+  }
+
+  handleDelete =() => {
+    ApiAdapter.deleteItem(this.props.castle.id)
+    .then(resp => this.props.updateCurrentUser(resp))
+  }
+
+  render() {
+    return (
+      <div className="castle-card">
+        <h2 className="castle-card-header">{this.props.castle.name}</h2>
+        <img src={this.props.castle.image} alt={this.props.castle.name} />
+        <button className="expand-castle" onClick={this.handleExpand}>
+          Expand Castle
+        </button>
+        <button className="delete-castle" onClick={this.handleDelete}>
+          Delete Castle
+        </button>
+      </div>
+    )
+  }
 }
-
-
