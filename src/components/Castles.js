589b486c09a4e4f664ff5620bef1d7afa3ee2cd9
Yoan Ante <yoanante@gmail.com>
2018-12-29 18:15:26 -0500

    Added updateCurrentuser and user prop to Castle JSX array

--- src/components/Castles.js
+++ src/components/Castles.js
@@ -4,7 +4,7 @@ import Castle from './Castle'
 class Castles extends Component {
 
   renderCastles = () => {
-    return this.props.castles.map(castle => <Castle castle={castle} key={castle.id}/>)
+    return this.props.castles.map(castle => <Castle castle={castle} key={castle.id} updateCurrentUser={this.props.updateCurrentUser} user={this.props.user} />)
   }
 
 
