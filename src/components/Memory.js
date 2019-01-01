729e8d231ae456fa99b770f7f89341f07f0d4e49
Yoan Ante <yoanante@gmail.com>
2018-12-31 20:22:16 -0500

    callbacks onClick to deleteMemory and updateMemory added delete memory button

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -4,10 +4,12 @@ import React from 'react';
 
 
 
-const Memory = ({ memory }) => {
+const Memory = ({ memory, deleteMemory, updateMemory}) => {
 
   return (
-  <div className ="memory" style={{left: `${memory.x}`, top: `${memory.y}` }}> {memory.text}</div>
+  <div className="memory" id={`memory-${memory.id}`} style={{left: `${memory.x}`, top: `${memory.y}` }} onClick={event => updateMemory(memory.id, event)}> {memory.text}
+  <button className="delete-memory" onClick={() => deleteMemory(memory.id)}>X</button>
+  </div>
   )
 }
 
