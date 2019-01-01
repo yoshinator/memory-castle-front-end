5b0496a3b6d8a9e6cc2c975e76615b220a4813ee
Yoan Ante <yoanante@gmail.com>
2018-12-31 20:21:10 -0500

    passing more props down to memory component.

--- src/components/Memories.js
+++ src/components/Memories.js
@@ -4,9 +4,9 @@ import Memory from './Memory'
 class Memories extends Component {
 
   renderMemories = () => {
-    return this.props.memories.map(memory => <Memory memory={memory} key={memory.id} />)
-  }
 
+    return this.props.memories.map(memory => <Memory memory={memory} key={memory.id} deleteMemory={this.props.deleteMemory} updateMemory={this.props.updateMemory}/>)
+  }
 
 
   render() {
