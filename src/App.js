5b0061794fd228c51b3c507bd577d5ec6ecae43d
Yoan Ante <yoanante@gmail.com>
2019-01-02 10:01:07 -0500

    Welcome div

--- src/App.js
+++ src/App.js
@@ -21,6 +21,14 @@ class App extends Component {
         <header className="App-header">
           Welcome to Memory Castle
         </header>
+        <div className="intro">
+          A Memory Palace, is an imaginary location in
+        your mind where you can store mnemonic images. The most common
+        type of memory palace involves making a journey through a place
+        you know well, like a building or town. Along that journey there
+        are specific locations that you always visit in the same order.
+          <a href="https://sansforgetica.rmit/" >On your journey you will see this crazy font. Its a font developed by MIT, it's scientifically designed to help you remember things. </a>
+        </div>
         <Users users={this.state.users}  ApiAdapter={ApiAdapter}/>
       </div>
     )
