65388b72b3294f587495d0519d20374e9dbe42a0
Yoan Ante <yoanante@gmail.com>
2019-01-03 20:42:27 -0500

    Google styling and oauth component update, not done

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+
+  state ={
+    isSignedIn: null
+  }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId:
           "389495129041-p4kq18b7qqeh0nceiccj3pjcs4lhrkh8.apps.googleusercontent.com",
         scope: "email"
+      }).then(()=>{
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        this.auth.isSignedIn.listen(this.onAuthChange)
       });
     });
   }
+
+
+  onAuthChange = () => {
+    this.setState({
+      isSignedIn: this.auth.isSignedIn.get()
+    })
+  }
+
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null){
+      return null
+    }
+    else if (this.state.isSignedIn){
+      return <button className="loginBtn loginBtn--google" >Log in with Google</button>;
+    }else {
+      return <div>You are not signed in</div>
+    }
+  }
+
+
   render() {
     return (
       <div>
-        Google Auth 
+        {this.renderAuthButton()}
       </div>
     )
   }
