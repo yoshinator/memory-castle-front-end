cfe63573f446a5b86c5a6c55a1ac9f01dd87010f
Yoan Ante <yoanante@gmail.com>
2019-01-03 20:41:30 -0500

    changed to class component and greate drag method.

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -1,17 +1,40 @@
-import React from 'react';
 
+import React, { Component } from 'react'
+class Memory extends Component {
 
+  constructor(props){
+    super(props);
+    this.state ={
+      id: this.props.memory.id,
+      x: this.props.memory.x,
+      y: this.props.memory.y,
+      text: this.props.memory.text
+    }
+  }
 
-
-
-const Memory = ({ memory, deleteMemory, updateMemory}) => {
-
-  return (
-  <div className="memory" id={`memory-${memory.id}`} style={{left: `${memory.x}`, top: `${memory.y}` }} onClick={event => updateMemory(memory.id, event)}> {memory.text}
-  <button className="delete-memory" onClick={() => deleteMemory(memory.id)}>X</button>
-  </div>
-  )
+  drag = (event, updateMemory) => {
+      const newObj = {
+        x: `${(event.clientX / window.innerWidth) * 100}%`,
+        y: `${(event.clientY / window.innerHeight) * 100}%`
+      }
+      let {x, y} = newObj
+      this.setState({x,y})
+      updateMemory(newObj, this.state.id)
+    }
+  render() {
+    return (
+      <div draggable="true" onDragEnd={event => this.drag(event, this.props.updateMemory)} className="memory" id={`memory-${this.state.id}`} style={{ left: `${this.state.x}`, top: `${this.state.y}` }} >
+        {" "}
+        {this.state.text}
+        <button className="delete-memory" onClick={() => this.props.deleteMemory(this.state.id)}>
+          X
+      </button>
+      </div>
+    )
+  }
 }
 
 
 export default Memory; 
+
+
